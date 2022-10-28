fde2b96b4b4a340a654fe8eb07158016479333cb
kohei <koheitech001@gmail.com>
2022-10-29 08:59:09 +0900

    update

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ APP_PORT = os.getenv('APP_PORT', 80)
 
 PATH_DATA = os.getenv('PATH_DATA', './temp')
 PATH_MODEL = os.getenv('PATH_MODEL', './model')
-MODEL_NAME = os.getenv('MODEL_NAME', './model.onnx')
+MODEL_NAME = os.getenv('MODEL_NAME', 'model.onnx')
 DELETE_INTERVAL = os.getenv('DELETE_INTERVAL', 60)
